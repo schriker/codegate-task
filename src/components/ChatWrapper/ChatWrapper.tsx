faf9c9a31540ee838adbf0d1b13c2fa6752a9cdc
Marcin Janus <janekmachine@gmail.com>
2022-04-03 14:22:39 +0200

    React 18 root render fixed

--- src/components/ChatWrapper/ChatWrapper.tsx
+++ src/components/ChatWrapper/ChatWrapper.tsx
@@ -2,7 +2,7 @@ import { ChatWrapperPropsType } from '../../types/chatWrapper';
 
 function ChatWrapper({ children }: ChatWrapperPropsType) {
   return (
-    <div className="flex flex-col flex-grow-0 mt-3 border border-zinc-200 rounded-t-xl overflow-hidden">
+    <div className="flex flex-col flex-auto  mt-3 border border-zinc-200 rounded-t-xl overflow-hidden">
       {children}
     </div>
   );
--- src/features/messages/Messages.tsx
+++ src/features/messages/Messages.tsx
@@ -5,7 +5,7 @@ import MessageInput from '../../components/MessageInput/MessageInput';
 function Messages() {
   return (
     <ChatWrapper>
-      <div className="overflow-auto">
+      <div className="overflow-auto flex-auto">
         <Message />
       </div>
       <MessageInput />
